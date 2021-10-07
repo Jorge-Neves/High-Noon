ca8b0cb3fc2f13c1530a9ad6f7fd5e24f7babef2
Jorge-Neves <paulo.jorge.ngs@gmail.com>
2021-10-07 13:09:25 +0100

    navbar and footer first fix

--- src/components/Navbars/NavBarLanding.js
+++ src/components/Navbars/NavBarLanding.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
-import"./NavBarMain.css"
+import"./NavBarLanding.css"
 
 function NavBarLanding() {
 
    return(
    
        <nav className="navi-l-bar" >
+        <div className="navi-l-bar-mini">
+            <img src="../../logo.png" className="landing-logo-size" alt="high noon logo" />
+            <span className="landing-logo-text-size">
+              High Noon
+           </span>
+        </div>
        <div>
        <NavLink activeStyle={{ color: "red" }} to="/signup">
-             Enter
+       <span className="landing-logo-text-size">
+              Enter
+           </span>
            </NavLink>
        </div>
        <div>
            <NavLink activeStyle={{ color: "red" }} exact to="/resources">
-            Resources
+           <span className="landing-logo-text-size">
+              Resources
+           </span>
+          </NavLink>
+      </div>
+      <div>
+           <NavLink activeStyle={{ color: "red" }} exact to="/resources">
+           <span className="landing-logo-text-size">
+              Meet the Team
+           </span>
           </NavLink>
       </div>
         
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -3,6 +3,8 @@ import { useHistory, NavLink } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "./Signup.css"
+import NavBarLanding from "./Navbars/NavBarLanding";
+import Footer from "./Navbars/Footer";
 
  function Signup({ setCurrentLoggedInUser }) {
   const [username, setUsername] = useState("");
@@ -61,92 +63,100 @@ import "./Signup.css"
   };
 
   return (
-    <div className="signup-bg">
-      <div className="row">
-    <div className="col-md-6 mx-auto p-0">
-        <div className="card">
-           
-            <div className="login-box">
-                <div className="login-snip"> 
-                <input id="tab-1" type="radio" name="tab" className="sign-in" checked/>
-                <label for="tab-1" className="tab">Login</label> 
-                <input id="tab-2" type="radio" name="tab" className="sign-up"/>
-                <label for="tab-2" className="tab">Sign Up</label>
-                    <div className="login-space">
-                        <div className="login">
-                        <form onSubtmit={handleFormSubmitLogin}>
-                            <div className="group"> 
-                    
-                              <label for="user" className="label">Username</label> 
-                              <input 
-                              id="user" 
-                              type="text" 
-                              className="input" 
-                              onChange={(e) => setUsernameLogin(e.target.value)}
-                              value={usernameLogin}
-                              /> 
-                            </div>
-                            <div className="group">
-                              <label for="pass" className="label">Password</label> 
-                              <input 
-                              id="pass" 
-                              type="password" 
-                              className="input" 
-                              data-type="password" 
-                              onChange={(e) => setPasswordLogin(e.target.value)}
-                              value={passwordLogin}
-                              /> 
-                               
-                            </div>
-                            
-                            <div className="group"> 
-                            <button type="submit" className="button">Sign In</button>
-                      
-                            </div>
-                            </form>
-                            <div className="hr">
-                            </div>
-                        </div>
-                     
-                      <form onSubmit={handleFormSubmit}>
-                        <div className="sign-up-form">
-                            <div className="group"> 
-                              <label for="user" className="label">Username</label> 
-                              <input 
-                              id="user" 
-                              type="text" 
-                              className="input" 
-                              onChange={(e) => setUsername(e.target.value)}
-                              value={username}/>
-                            </div>
-                            <div className="group"> 
-                              <label for="pass" className="label">Password</label> 
-                              <input 
-                              id="pass" 
-                              type="password" 
-                              className="input" 
-                              data-type="password" 
-                              onChange={(e) => setPassword(e.target.value)}
-                              value={password}/> 
-                            </div>
-                            <div className="group"> 
-                              <input type="submit" className="button" value="Sign Up"/> 
-                            </div>
-                            <div className="hr"></div>
-                            <div className="foot"> 
-                              <label for="tab-1">Already Member?</label> 
-                            </div>
-                        </div>
-                      </form>
+    <div className="signup-bg row-vertical-signup">
+       <div className="row-signup">
+                    <div>
+                        <NavBarLanding />
                     </div>
-                </div>
             </div>
-        </div>
-    </div>
+      <div className="row-signup">
+      <div className="col-md-6 mx-auto p-0">
+          <div className="card">
+            
+              <div className="login-box">
+                  <div className="login-snip"> 
+                  <input id="tab-1" type="radio" name="tab" className="sign-in" checked/>
+                  <label for="tab-1" className="tab">Login</label> 
+                  <input id="tab-2" type="radio" name="tab" className="sign-up"/>
+                  <label for="tab-2" className="tab">Sign Up</label>
+                      <div className="login-space">
+                          <div className="login">
+                          <form onSubtmit={handleFormSubmitLogin}>
+                              <div className="group"> 
+                      
+                                <label for="user" className="label">Username</label> 
+                                <input 
+                                id="user" 
+                                type="text" 
+                                className="input" 
+                                onChange={(e) => setUsernameLogin(e.target.value)}
+                                value={usernameLogin}
+                                /> 
+                              </div>
+                              <div className="group">
+                                <label for="pass" className="label">Password</label> 
+                                <input 
+                                id="pass" 
+                                type="password" 
+                                className="input" 
+                                data-type="password" 
+                                onChange={(e) => setPasswordLogin(e.target.value)}
+                                value={passwordLogin}
+                                /> 
+                                
+                              </div>
+                              
+                              <div className="group"> 
+                              <button type="submit" className="button">Sign In</button>
+                        
+                              </div>
+                              </form>
+                              <div className="hr">
+                              </div>
+                          </div>
+                      
+                        <form onSubmit={handleFormSubmit}>
+                          <div className="sign-up-form">
+                              <div className="group"> 
+                                <label for="user" className="label">Username</label> 
+                                <input 
+                                id="user" 
+                                type="text" 
+                                className="input" 
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}/>
+                              </div>
+                              <div className="group"> 
+                                <label for="pass" className="label">Password</label> 
+                                <input 
+                                id="pass" 
+                                type="password" 
+                                className="input" 
+                                data-type="password" 
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}/> 
+                              </div>
+                              <div className="group"> 
+                                <input type="submit" className="button" value="Sign Up"/> 
+                              </div>
+                              <div className="hr"></div>
+                              <div className="foot"> 
+                                <label for="tab-1">Already Member?</label> 
+                              </div>
+                          </div>
+                        </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div className="row-signup">
+                    <div>
+                        <Footer />
+                    </div>
+            </div>
 </div>
-
-       
-    </div>
   );
 
 
