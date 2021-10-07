8be74feb87d3c36f6e7501de63fd6e30d92cb6b7
Gregory Marklew <greg_marklew@hotmail.com>
2021-10-07 09:43:34 +0100

    card icons

--- src/components/Greg.js
+++ src/components/Greg.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./Contacts.css"
+import "./Contacts.css";
+import "font-awesome/css/font-awesome.min.css";
 
 function Greg() {
   return (
@@ -10,50 +11,34 @@ function Greg() {
     // </div>
     <>
       <div class="container-personal">
-    
-    <div class="card-wrapper-personal">
-      
-      <div class="card-personal">
-        
-        <div class="card-image-personal">
-          <img src="/greg.png" alt="Web Dev Greg"/>
+        <div class="card-wrapper-personal">
+          <div class="card-personal">
+            <div class="card-image-personal">
+              <img className="card-img-pers" src="/greg.png" alt="Web Dev Greg" />
+            </div>
+
+            <ul class="social-icons">
+              <li>
+                <a href="https://www.linkedin.com/in/gregory-marklew/" target="_blank">
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/NoisyOgre" target="_blank">
+                  <i className="fa fa-github"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="details ">
+              <h2>
+                Gregory Marklew
+                <br />
+                <span class="job-title">Web Developer</span>
+              </h2>
+            </div>
+          </div>
         </div>
-
-      <ul class="social-icons">
-        <li>
-          <a href="">
-            <i class="fab fa-facebook-f"></i>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <i class="fab fa-instagram"></i>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <i class="fab fa-dribbble"></i>
-          </a>
-        </li>
-      </ul>
-
-      <div class="details ">
-        <h2>Gregory Marklew
-          <br/>
-          <span class="job-title">Web Developer</span>
-        </h2>
       </div>
-    </div>
-  </div>
-    
-    
-
- </div>
     </>
   );
 }
